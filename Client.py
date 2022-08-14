22bb30ea0a38c338bc28910acc91f40e9fab9df2
Zachariah Dunn <zacdunn2000@hotmail.com>
2022-08-14 13:19:13 -0300

    Working on client issues

--- Client.py
+++ Client.py
@@ -23,28 +23,3 @@ async def handleClient():
 
 if __name__ == "__main__":
     asyncio.run(handleClient())
-
-
-# async def client(websocket):
-
-    # coords = await websocket.recv()
-    # print(f"<<< {coords}")
-
-    # # x = str(coords).split(",")[0]
-    # # y = str(coords).split(",")[1]
-
-    # # pygame.event.post(pygame.event.Event(
-    # # EventTypes.CREATE_SHAPE, coords=(x, y)))
-
-# async def start():
-#     uri = "ws://localhost:8765"
-
-#     async for websocket in websockets.connect(uri):
-#         try:
-#             await client(websocket)
-#         except websocket.ConnectionClosed:
-#             continue
-
-# if __name__ == "__main__":
-
-#     asyncio.run(start())
--- ClientGame.py
+++ ClientGame.py
@@ -34,6 +34,11 @@ def startGame():
             pygame.quit()
             sys.exit()
 
+        if len(pygame.event.get(pygame.WINDOWMOVED)) > 0:
+            all_sprites.empty()
+            pygame.event.pump()
+            continue
+
         displaysurface.fill((0, 0, 0))
 
         for entity in all_sprites:
@@ -42,23 +47,33 @@ def startGame():
         pygame.display.update()
         FramePerSec.tick(FPS)
 
-        all_sprites.empty()
-
         for event in pygame.event.get(EventTypes.CREATE_SHAPE):
             for shape in str(event.shapes).split(";"):
+                alreadyExists = False
+                
                 try:
                     name = str(shape.split(",")[0])
                     xPos = int(shape.split(",")[1])
                     yPos = int(shape.split(",")[2])
                     radi = int(shape.split(",")[3])
-
-                    shap = Shape(name, xPos, yPos, radi)
-                    shap.move()
-                    all_sprites.add(shap)
                 except:
                     continue
 
-        #physics.update(all_sprites)
+                # Check all existing shapes to make sure there isn't one already with the same UUID
+                for shape2 in all_sprites:
+                    # If the shape already exists
+                    if shape2.name == name:
+                        alreadyExists = True
+                        shape2.pos.x = xPos
+                        shape2.pos.y = yPos
+                        shape2.radius = radi
+                        shap.move()
+
+                # If the shape doesn't exist
+                if (alreadyExists == False or len(all_sprites) == 0):
+                    shap = Shape(name, xPos, yPos, radi)
+                    shap.move()
+                    all_sprites.add(shap)
 
         # let pygame handle events we don't process
         pygame.event.pump()
