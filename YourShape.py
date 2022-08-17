65e7d35295e56cf6b6fe2d2fbea612f29351fdb1
Adam Mattatall <adam.mattatall@garvin-allen.com>
2022-08-17 09:14:45 -0300

    stuf

--- YourShape.py
+++ YourShape.py
@@ -8,17 +8,17 @@ class YourShape(Shape):
         super().__init__()
 
         # # The Name of your shape
-        # self.name = ""
+        self.name = "Adam"
 
         # # The color of your shape
         # # (Red, Blue, Green)
         # self.color = (0, 0, 0)
 
         # # How fast your shape is
-        # self.acc = vec(5, 5)
+        self.acc = vec(5, 5)
 
         # # How big yuour shape is
-        # self.radius = 100
+        self.radius = 50
 
         # # Where your shape in the game
         # self.pos = vec((200, 200))
